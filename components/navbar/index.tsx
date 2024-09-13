c4a416e88f0e7b48d6801656c6b2c192c64a56b6
helloanime <helloanime007@gmail.com>
2024-09-13 10:43:01 -0700

    yoyooo

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import HomeNavBar from "./homeNav";
 import InfoNavBar from "./infoNav";
 import SearchNavBar from "./searchNav";
-import SearchNavBar from "./blog";
+import blog from "./blog";
 
 type NavBarProps = {
   navFor?: "home" | "info" | "search";
